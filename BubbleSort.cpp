f0a50a36b74c7263724cecc8131b93c3258e3d68
Taqi haider <taqihaider591@gmail.com>
2023-06-22 20:20:40 +0500

    bubblesort

---
+++ BubbleSort.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+void bubblesort(int arr[],int size){
+    for(int i=0;i<size;i++){
+        for(int j=0;j<size-1-i;j++){
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+
+}
+int main(){
+    int s;
+    cout<<"Enter the size of an array\n";
+    cin>>s;
+    int arr[s];
+    for(int i=0;i<s;i++){
+        cout<<"Enter the element of index " << i<< " of array: "<<endl;
+        cin>>arr[i];
+    }
+    cout<<"Original Array:-"<<endl;
+    for(int i=0;i<s;i++){
+        cout<<arr[i]<<" ";
+    }
+    bubblesort(arr,s);
+    cout<<"\nAfter Sorting:-"<<endl;
+    for(int i=0;i<s;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
