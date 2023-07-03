1a4d1eff5bf74c56a479f794457b72f9b5e28778
Taqi haider <taqihaider591@gmail.com>
2023-07-03 23:13:47 +0500

    Palindrome.cpp

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -1,6 +1,24 @@
 #include<iostream>
 #include<stdbool.h>
+#include<cstring>
 using namespace std;
+//For String
+bool Palindrome(string str){
+    string rev;
+    int len=str.length();
+    for(int i=len-1;i>=0;i--){
+        rev += str[i];
+    }
+    if (rev == str) {
+        cout << "The string is a palindrome" << endl;
+        return true;
+    } else {
+        cout << "Not a palindrome" << endl;
+        return false;
+    }
+}
+
+//For Int
 bool isPalindrome(int x){
     int rev=0;
     int temp=x;
@@ -19,8 +37,12 @@ bool isPalindrome(int x){
 }
 int main(){
     int x;
+    string x1;
     cout<<"Enter no to check:"<<endl;
     cin>>x;
     isPalindrome(x);
+    cout<<"Enter string to check:"<<endl;
+    cin>>x1;
+    Palindrome(x1);
     return 0;
 }
