57b9f283a2010311ecdf16f5d2cc3e43ac5947d6
taqihaider591 <taqihaider591@gmail.com>
2024-03-22 01:13:03 +0500

    theMaximumAchievableX

--- array/FindTheMaximalAchievable no.cpp
+++ array/FindTheMaximalAchievable no.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     int theMaximumAchievableX(int num, int t) {
         int x;
-        x = num + 2*t;
+        // x = num + 2*t;
+        x = num + t + t;
         return x;
     }
 };
