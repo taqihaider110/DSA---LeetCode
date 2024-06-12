7db182dd18a93bcbd000366270fadbcbda8dc066
taqihaider591 <taqihaider591@gmail.com>
2024-06-13 01:46:37 +0500

    Unique Number of Occurences

---
+++ Hash Table/C++/Unique Number of Occurences.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        map<int, int> mp;
+        for (auto x : arr) {
+            mp[x]++;
+        }
+        vector<int> v;
+        for (auto x : mp) {
+            v.push_back(x.second);
+        }
+        sort(v.begin(), v.end());
+        for (int i = 0; i < v.size() - 1; i++) {
+            if (v[i] == v[i + 1])
+                return false;
+        }
+        return true;
+    }
+};
