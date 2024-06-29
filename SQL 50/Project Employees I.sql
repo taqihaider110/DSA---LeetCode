b94c653dbe5406f362fd1c74fb46a93d36fb07ab
taqihaider591 <taqihaider591@gmail.com>
2024-06-29 15:56:40 +0500

    Project Employees I

---
+++ SQL 50/Project Employees I.sql
@@ -0,0 +1,9 @@
+Select 
+    Project.project_id, 
+    Round(Avg(Employee.experience_years), 2) as average_years
+from
+    Project left join Employee
+on 
+    Project.employee_id = Employee.employee_id 
+group by 
+    Project.project_id;
