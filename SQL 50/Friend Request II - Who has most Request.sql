24abf5b00f1c545b3d5b82b349a7e84e7b9cbaa1
taqihaider591 <taqihaider591@gmail.com>
2024-07-01 00:27:04 +0500

    Friend Request II - Who has most Request

---
+++ SQL 50/Friend Request II - Who has most Request.sql
@@ -0,0 +1,11 @@
+with new as (
+(select accepter_id as id, count(*) as num from RequestAccepted
+group by 1)
+union all
+(select requester_id as id, count(*) as num from RequestAccepted
+group by 1)
+)
+
+select id, sum(num) as num from new
+group by id
+order by 2 desc limit 1
