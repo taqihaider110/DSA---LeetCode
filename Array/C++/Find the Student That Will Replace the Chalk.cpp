029b48e591fa02a0186e68ad223e50dc82588241
taqihaider591 <taqihaider591@gmail.com>
2024-09-11 00:28:40 +0500

    Find the Student That Will Replace the Chalk

---
+++ Array/C++/Find the Student That Will Replace the Chalk.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int chalkReplacer(vector<int>& chalk, int k) {
+        long long total = accumulate(begin(chalk),end(chalk),0LL);
+        k %= total;
+        for(int i =0;i<chalk.size();i++){
+            if(chalk[i]>k){
+                return i;
+            }
+            else{
+                k-=chalk[i];
+            }
+        }
+        return -1;
+    }
+};
