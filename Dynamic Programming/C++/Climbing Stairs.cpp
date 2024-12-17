d46c36a3e19f274f9bb740aae4d9621a8e7d0478
taqihaider110 <taqihaider591>
2024-12-17 20:55:31 +0500

    Climbing Stairs

---
+++ Dynamic Programming/C++/Climbing Stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n == 1 || n == 2) {
+            return n;
+        }
+        int a = 1 , b = 2;
+        int res = 0;
+        for(int i =3 ;i<=n;i++){
+            res = a+b;
+            a = b;
+            b = res;
+        }
+        return res;
+    }
+};
