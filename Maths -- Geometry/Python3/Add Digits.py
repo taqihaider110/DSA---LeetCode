edd60321b85c6782462e25f23fea4aeb00afb67c
Muhammad Taqi Haider <taqihaider591@gmail.com>
2026-07-01 13:59:36 +0500

    Add Digits solution

---
+++ Maths -- Geometry/Python3/Add Digits.py
@@ -0,0 +1,11 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        sum = 0
+        while num:
+            sum += num % 10
+            num //=10
+
+        if sum < 10:
+            return sum
+        else:
+            return self.addDigits(sum)
